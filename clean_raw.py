b1daca39dc37e63b2c030badcddaf89fea7bfd2a
shiftout <shiftout@protonmail.com>
2019-02-03 15:48:56 +0100

    Update clean_raw.py

--- clean_raw.py
+++ clean_raw.py
@@ -1,33 +1,77 @@
 import glob, os, argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--path', help= 'path to a JPG/JPEG directory')
-args = parser.parse_args()
+def get_jpg_files(directory):
+    jpgs = set()
+    
+    for file in glob.glob(os.path.join(directory,"*")):
+        if (file.lower().endswith(".jpg") or file.lower().endswith(".jpeg")):
+            jpgs.add(file)
+    
+    return jpgs
 
-if args.path is not None:
-    os.chdir(args.path)
+def get_cr2_files(directory):
+    cr2s = set()
+    
+    for file in glob.glob(os.path.join(directory,"*")):
+        if (file.lower().endswith(".cr2")):
+            cr2s.add(file)
+    
+    return cr2s
 
-cwd = os.getcwd()
-print("Working directory: {}".format(cwd))
+def move_raw_files(src_dir, dest_dir):
+    cnt = 0
+    for file in get_cr2_files(src_dir):
+        base = os.path.basename(file)
+        
+        dest_file = os.path.join(dest_dir, base)
+        if os.path.exists(dest_file):
+            os.remove(dest_file)
+        
+        os.rename(file, dest_file)
+        cnt += 1
 
-jpgs = set()
-for file in glob.glob("*"):
-    if (file.lower().endswith(".jpg") or file.lower().endswith(".jpeg")):
-        jpgs.add(file.split('.')[0])
+    return cnt
 
-if (len(jpgs) == 0):
-    print("No images (JPG, JPEG) were found. Please run on a different directory or specify --path as an argument.")
-    raise SystemExit
+def remove_orphan_raw_files(jpg_dir, raw_dir):
+    
+    jpgnames = set()
+    for file in get_jpg_files(jpg_dir):
+        base = os.path.basename(file)
+        jpgnames.add(base.split('.')[0])
 
-print("{} JPG/JPEG images found".format(len(jpgs)))
-# print(", ".join(str(e) for e in jpgs))
+    cnt = 0
+    for file in get_cr2_files(raw_dir):
+        base = os.path.basename(file)
+        if base.split('.')[0] not in jpgnames:
+            os.remove(file)
+            cnt += 1
+    
+    return cnt  
 
-del_cnt = 0
-size_cnt = 0
-for file in glob.glob("*"):
-    if (file.lower().endswith(".cr2") and file.split('.')[0] not in jpgs):
-        del_cnt += 1
-        size_cnt += os.stat(file).st_size >> 20
-        os.remove(file)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', help= 'path to a JPG/JPEG directory')
+    args = parser.parse_args()
 
-print("{} raw files removed. {}MB freed up.".format(del_cnt, size_cnt))
+    if args.path is not None:
+        os.chdir(args.path)
+
+    cwd = os.getcwd()
+    print("Working directory: {}".format(cwd))
+
+    jpgs = get_jpg_files(cwd)
+    if (len(jpgs) == 0):
+        print("No images (JPG, JPEG) were found. Please run on a different directory or specify --path as an argument. Exiting.")
+        raise SystemExit
+    print("{} JPG/JPEG files found".format(len(jpgs)))
+
+    raw_dir = os.path.join(cwd, "raw")
+    if not os.path.exists(raw_dir):
+        os.makedirs(raw_dir)
+        print("Directory created {}".format(raw_dir))
+    
+    print("{} raw files moved".format(move_raw_files(cwd, raw_dir)))
+    print("{} orphan raw files removed".format(remove_orphan_raw_files(cwd, raw_dir)))
+
+if __name__ == "__main__":
+    main()
